4b670d513d5a3a8ae2cc97944c2bfa25a45ca4cd
段lizhi <lizhi_duan@126.com>
2018-07-27 08:48:49 +0800

    '测试类'

---
+++ transform/strings_test.go
@@ -0,0 +1,13 @@
+/**
+ * <p>Description: (一句话描述一下该文件的作用) </>
+ * @author lizhi_duan
+ * @date 2018/7/27 08:45
+ * @version 1.0
+ */
+package transform
+
+import "testing"
+
+func TestBaseType2String(t *testing.T) {
+
+}
